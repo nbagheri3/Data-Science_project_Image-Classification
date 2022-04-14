fba7b817eafacacc8d14b6c8e3dafaaff36995d0
Nahal Bagheri <91636662+nbagheri3@users.noreply.github.com>
2022-04-13 23:37:55 -0700

    Add files via upload

---
+++ Hello-World.java
@@ -0,0 +1,12 @@
+/**
+ * Name: Nahal Bagheri
+ * This program print out "Hello World"
+ * Introducing Print funstion and System Class.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+	// Printing "Hello World"
+        System.out.println("Hello World");
+    }
+}
